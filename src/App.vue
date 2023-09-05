ac5e52996d4af8ecc37b1c27ad0a1a007f24614f
vohidiprofticket <vohidiprofticket@gmail.com>
2023-09-05 17:24:36 +0300

    add project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+
+const CHARACTERS_QUERY = gql`
+  query Characters {
+    characters {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`
+const { result, loading, error } = useQuery(CHARACTERS_QUERY);
+
+console.log(result.value, "result", error.value, loading.value)
+</script>
+
+<template>
+  <p v-if="error">Error...</p>
+  <p v-if="loading">Loading...</p>
+  <!-- <p v-else v-for="character in result.characters.results" :key="character.id">
+    {{ character.name }}
+  </p> -->
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/view/MainComponent.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="main">
+        <div class="header">
+
+        </div>
+        <div class="content">
+            <div class="transactions">
+                <div class="transactions-data">
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>
+                                    <span class="table-heading">
+                                        Txn Hash	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-heading">
+                                        Age
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-heading">
+                                        From	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-heading">
+                                        To	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-heading">
+                                        Value	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-heading">
+                                        Token	
+                                    </span>
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <span class="table-body">
+                                        Txn Hash	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-body">
+                                        Age
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-body">
+                                        From	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-body">
+                                        To	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-body">
+                                        Value	
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="table-body">
+                                        Token	
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss">
+    
+</style>
